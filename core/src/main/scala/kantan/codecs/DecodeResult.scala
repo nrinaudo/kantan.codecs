b31ac279dd1d21504e2bb242a46b0b1d2c385b83
nrinaudo <nicolas@nrinaudo.com>
2016-02-14 21:10:09 +0100

    Add further useful methods to DecodeResult.
    This will need some testing...

--- core/src/main/scala/kantan/codecs/DecodeResult.scala
+++ core/src/main/scala/kantan/codecs/DecodeResult.scala
@@ -1,5 +1,7 @@
 package kantan.codecs
 
+import scala.util.Try
+
 /** Represents the result of a decode operation
   *
   * @tparam F failure type.
@@ -17,6 +19,19 @@ sealed abstract class DecodeResult[+F, +S] extends Product with Serializable {
   def orElse[F2 >: F, S2 >: S](default: ⇒ DecodeResult[F2, S2]): DecodeResult[F2, S2]
   def getOrElse[S2 >: S](default: ⇒ S2): S2
   def get: S
+
+
+
+  // - Standard Scala operations ---------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
+  def foreach(f: S ⇒ Unit): Unit
+  def recover[SS >: S](pf: PartialFunction[F, SS]): DecodeResult[F, SS]
+  def recoverWith[SS >: S, FF >: F](pf: PartialFunction[F, DecodeResult[FF, SS]]): DecodeResult[FF, SS]
+  def valueOr[SS >: S](f: F ⇒ SS): SS
+  def forall(f: S ⇒ Boolean): Boolean
+  def exists(f: S ⇒ Boolean): Boolean
+  def ensure[FF >: F](fail: ⇒ FF)(f: S ⇒ Boolean): DecodeResult[FF, S]
+
 
 
   // - Foldable operations ---------------------------------------------------------------------------------------------
@@ -51,19 +66,26 @@ sealed abstract class DecodeResult[+F, +S] extends Product with Serializable {
   // -------------------------------------------------------------------------------------------------------------------
   def toOption: Option[S]
   def toEither: Either[F, S]
+  def toList: List[S]
 }
 
 object DecodeResult {
   // - Instance construction -------------------------------------------------------------------------------------------
   // -------------------------------------------------------------------------------------------------------------------
-  def apply[S](s: ⇒ S): DecodeResult[Exception, S] =
-    try { Success(s) }
-    catch { case e: Exception ⇒ Failure(e) }
+  def catchNonFatal[S](s: ⇒ S): DecodeResult[Throwable, S] =
+    try {success(s)}
+    catch { case scala.util.control.NonFatal(t) ⇒ failure(t) }
 
-  def apply[F, S](s: ⇒ S, f: ⇒ F): DecodeResult[F, S] = DecodeResult(s).leftMap(_ ⇒ f)
+  def fromTry[S](t: Try[S]): DecodeResult[Throwable, S] = t match {
+    case scala.util.Success(s) ⇒ success(s)
+    case scala.util.Failure(f) ⇒ failure(f)
+  }
 
-  def success[S](s: S): DecodeResult[Nothing, S] = Success(s)
-  def failure[F](f: F): DecodeResult[F, Nothing] = Failure(f)
+  def fromEither[F, S](e: Either[F, S]): DecodeResult[F, S] = e.fold(failure, success)
+  def fromOption[F, S](o: Option[S], f: ⇒ F): DecodeResult[F, S] = o.fold(failure[F, S](f))(success)
+
+  def success[F, S](s: S): DecodeResult[F, S] = Success(s)
+  def failure[F, S](f: F): DecodeResult[F, S] = Failure(f)
 
 
 
@@ -78,6 +100,14 @@ object DecodeResult {
 
     override def fold[C](ff: Nothing => C, fs: S => C): C = fs(value)
 
+    override def foreach(f: S => Unit) = f(value)
+    override def recover[SS >: S](pf: PartialFunction[Nothing, SS]) = this
+    override def recoverWith[SS >: S, FF >: Nothing](pf: PartialFunction[Nothing, DecodeResult[FF, SS]]) = this
+    override def valueOr[SS >: S](f: Nothing => SS) = value
+    override def forall(f: S => Boolean) = f(value)
+    override def ensure[FF >: Nothing](fail: => FF)(f: S => Boolean) = if(f(value)) this else failure(fail)
+    override def exists(f: S => Boolean) = f(value)
+
     override def map[S2](f: S => S2) = copy(value = f(value))
     override def flatMap[F2 >: Nothing, S2](f: S => DecodeResult[F2, S2]) = f(value)
 
@@ -86,6 +116,7 @@ object DecodeResult {
 
     override def toOption = Some(value)
     override def toEither = Right(value)
+    override def toList = List(value)
   }
 
 
@@ -101,6 +132,18 @@ object DecodeResult {
 
     override def fold[C](ff: F => C, fs: Nothing => C): C = ff(value)
 
+    override def foreach(f: Nothing => Unit): Unit = ()
+    override def recover[SS >: Nothing](pf: PartialFunction[F, SS]) =
+      if(pf.isDefinedAt(value)) success(pf(value))
+      else                      this
+    override def recoverWith[SS >: Nothing, FF >: F](pf: PartialFunction[F, DecodeResult[FF, SS]]) =
+      if(pf.isDefinedAt(value)) pf(value)
+      else                      this
+    override def valueOr[SS >: Nothing](f: F => SS) = f(value)
+    override def forall(f: Nothing => Boolean) = true
+    override def ensure[FF >: F](fail: => FF)(f: Nothing => Boolean) = this
+    override def exists(f: Nothing => Boolean) = false
+
     override def map[S](f: Nothing => S) = this
     override def flatMap[F2 >: F, S](f: Nothing => DecodeResult[F2, S]) = this
 
@@ -109,5 +152,6 @@ object DecodeResult {
 
     override def toOption = None
     override def toEither = Left(value)
+    override def toList = Nil
   }
 }
