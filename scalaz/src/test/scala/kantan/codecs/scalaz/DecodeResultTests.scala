57dd3d570607da1ea57facb422661b165cd8ac94
nrinaudo <nicolas@nrinaudo.com>
2016-02-15 18:10:51 +0100

    Add tests for semigroup and monoid.

--- scalaz/src/test/scala/kantan/codecs/scalaz/DecodeResultTests.scala
+++ scalaz/src/test/scala/kantan/codecs/scalaz/DecodeResultTests.scala
@@ -3,12 +3,17 @@ package kantan.codecs.scalaz
 import kantan.codecs.DecodeResult
 import kantan.codecs.laws.discipline.arbitrary._
 
-import scalaz.scalacheck.ScalazProperties.{traverse, equal, monad}
+import scalaz.{Semigroup, NonEmptyList}
+import scalaz.scalacheck.ScalazProperties._
+import scalaz.scalacheck.ScalazArbitrary._
 import scalaz.std.anyVal._
 import scalaz.std.string._
+
 
 class DecodeResultTests extends ScalazSuite {
   checkAll("DecodeResult[String, ?]", traverse.laws[DecodeResult[String, ?]])
   checkAll("DecodeResult[String, ?]", monad.laws[DecodeResult[String, ?]])
   checkAll("DecodeResult[String, Int]", equal.laws[DecodeResult[String, Int]])
+  checkAll("DecodeResult[String, Int]", monoid.laws[DecodeResult[String, Int]])
+  checkAll("DecodeResult[String, NonEmptyList[Int]]", semigroup.laws[DecodeResult[String, NonEmptyList[Int]]])
 }
