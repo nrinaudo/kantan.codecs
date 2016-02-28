7d4e3b591338bac9ec46e9740c99dbff46049aad
nrinaudo <nicolas@nrinaudo.com>
2016-02-28 21:29:13 +0100

    Fix naming conflict.

--- laws-scalaz/src/main/scala/kantan.codecs.scalaz/laws/package.scala
+++ laws-scalaz/src/main/scala/kantan.codecs.scalaz/laws/package.scala
@@ -5,7 +5,7 @@ import org.scalacheck.Arbitrary
 
 import scalaz.Equal
 
-package object laws extends ScalazInstances {
+package object laws extends ArbitraryInstances {
   implicit def decoderEqual[E: Arbitrary, D: Equal, F: Equal, R[DD] <: Decoder[E, DD, F, R]]: Equal[R[D]] = new Equal[R[D]] {
     override def equal(a1: R[D], a2: R[D]) =
       kantan.codecs.laws.discipline.equality.eq(a1.decode, a2.decode) { (d1, d2) ⇒
