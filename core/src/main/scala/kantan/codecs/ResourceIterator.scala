8b453415089e129d0f1a534da1ffd233f9d1ed0c
nrinaudo <nicolas@nrinaudo.com>
2016-07-05 22:46:21 +0200

    Add ResourceIterator and tests.

---
+++ core/src/main/scala/kantan/codecs/ResourceIterator.scala
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2016 Nicolas Rinaudo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kantan.codecs
+
+import java.io.Closeable
+
+trait ResourceIterator[+A] extends TraversableOnce[A] with Closeable { self ⇒
+  // Makes sure the underlying resource is closed if empty.
+  // This is necessary for edges cases where hasNext returns true from the get-go: next is never called and we never
+  // check whether the underlying resource should be freed.
+  closeIfNeeded()
+
+
+
+  // - Internal state handling -----------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
+  protected def readNext(): A
+  protected def shouldClose: Boolean = !hasNext
+
+  private var isClosed = false
+
+  private def closeIfNeeded(): Unit = if(!isClosed && shouldClose) {
+    isClosed = true
+    close()
+  }
+
+  def close(): Unit
+  def hasNext: Boolean
+
+  final def next(): A =
+    try { readNext() }
+    finally { closeIfNeeded() }
+
+
+
+  // - Useful methods --------------------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
+  def drop(n: Int): ResourceIterator[A] =
+    if(n > 0 && hasNext) {
+      next()
+      drop(n - 1)
+    }
+    else this
+
+  def dropWhile(p: A ⇒ Boolean): ResourceIterator[A] =
+  // Empty rows: nothing to drop
+    if(isEmpty) this
+    else {
+      // Looks for the first element that does not match p.
+      var n = self.next()
+      while(self.hasNext && p(n)) n = self.next()
+
+      // No such element, return the empty stream.
+      if(isEmpty && p(n)) this
+
+      // We've found one such element, return a new iterator that starts with it.
+      else new ResourceIterator[A] {
+        var done = false
+
+        override def hasNext = !done || self.hasNext
+        override def close() = self.close()
+
+        override def readNext(): A =
+          if(done) self.readNext()
+          else {
+            done = true
+            n
+          }
+      }
+    }
+
+  def take(n: Int): ResourceIterator[A] = new ResourceIterator[A] {
+    var count = n
+    override def hasNext = count > 0 && self.hasNext
+    override def readNext() = {
+      if(count > 0) {
+        val a = self.readNext()
+        count -= 1
+        a
+      }
+      else ResourceIterator.empty.next()
+    }
+    override def close() = self.close()
+  }
+
+  // TODO: takeWhile
+
+  def collect[B](f: PartialFunction[A, B]): ResourceIterator[B] = {
+    var n = self.find(f.isDefinedAt)
+
+    new ResourceIterator[B] {
+      override def hasNext = n.isDefined
+
+      override def readNext() = {
+        val r = n.getOrElse(ResourceIterator.empty.next())
+        n = self.find(f.isDefinedAt)
+        f(r)
+      }
+
+      override def close() = self.close()
+    }
+  }
+
+
+
+  // - Monadic operations ----------------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
+  def map[B](f: A ⇒ B): ResourceIterator[B] = new ResourceIterator[B] {
+    override def hasNext  = self.hasNext
+    override def readNext() = f(self.readNext())
+    override def close()  = self.close()
+  }
+
+  def flatMap[B](f: A ⇒ ResourceIterator[B]): ResourceIterator[B] = {
+    var cur: ResourceIterator[B] = ResourceIterator.empty
+    new ResourceIterator[B] {
+      override def hasNext = cur.hasNext || self.hasNext && {cur = f(self.next()); hasNext}
+      override def readNext() = cur.readNext()
+      override def close() = self.close()
+    }
+  }
+
+  def filter(p: A ⇒ Boolean): ResourceIterator[A] = collect {
+    case a if p(a) ⇒ a
+  }
+
+  def withFilter(p: A ⇒ Boolean): ResourceIterator[A] = filter(p)
+
+
+  // - TraversableOnce -------------------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
+  override def foreach[U](f: A ⇒ U): Unit = while(hasNext) f(next())
+  override def seq: TraversableOnce[A] = this
+  override def hasDefiniteSize: Boolean = isEmpty
+
+  override def copyToArray[B >: A](xs: Array[B], start: Int, len: Int): Unit = {
+    var i = start
+    val end = start + math.min(len, xs.length - start)
+    while(i < end && hasNext) {
+      xs(i) = next()
+      i += 1
+    }
+  }
+
+  override def forall(p: A ⇒ Boolean): Boolean = {
+    var res = true
+    while(res && hasNext) res = p(next())
+    res
+  }
+  override def toTraversable: Traversable[A] = toStream
+  override def isEmpty: Boolean = !hasNext
+  override def find(p: A ⇒ Boolean): Option[A] = {
+    var res: Option[A] = None
+    while(res.isEmpty && hasNext) {
+      val n = next()
+      if(p(n)) res = Some(n)
+    }
+    res
+  }
+  override def exists(p: A ⇒ Boolean): Boolean = {
+    var res = false
+    while(!res && hasNext) res = p(next())
+    res
+  }
+  override def toStream: Stream[A] = if(hasNext) Stream.cons(next(), toStream) else Stream.empty
+  override def toIterator: Iterator[A] = new Iterator[A] {
+    override def hasNext: Boolean = self.hasNext
+    override def next(): A = self.next()
+  }
+  override def isTraversableAgain: Boolean = false
+
+  def withClose(f: () ⇒ Unit): ResourceIterator[A] = new ResourceIterator[A] {
+    override def hasNext = self.hasNext
+    override def readNext() = self.readNext()
+    override def close() = f()
+  }
+}
+
+object ResourceIterator {
+  val empty: ResourceIterator[Nothing] = new ResourceIterator[Nothing] {
+    override def hasNext = false
+    override def readNext() = throw new NoSuchElementException("next on empty resource iterator")
+    override def close() = ()
+  }
+
+  def apply[A](as: A*): ResourceIterator[A] = ResourceIterator.fromIterator(as.iterator)
+
+  def fromIterator[A](as: Iterator[A]): ResourceIterator[A] = new ResourceIterator[A] {
+    override def hasNext = as.hasNext
+    override def readNext() = as.next()
+    override def close() = ()
+  }
+}
---
+++ tests/src/test/scala/kantan/codecs/ResourceIteratorTests.scala
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2016 Nicolas Rinaudo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kantan.codecs
+
+import org.scalatest.FunSuite
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+class ResourceIteratorTests extends FunSuite with GeneratorDrivenPropertyChecks {
+  test("drop should drop the expected number of elements") {
+    forAll { (is: List[Int], n: Int) ⇒
+      assert(ResourceIterator(is:_*).drop(n).toList == is.drop(n))
+    }
+  }
+
+  test("dropWhile should drop the expected elements") {
+    forAll { (is: List[Int], f: Int ⇒ Boolean) ⇒
+      assert(ResourceIterator(is:_*).dropWhile(f).toList == is.dropWhile(f))
+    }
+  }
+
+  test("take should take the expected number of elements") {
+    forAll { (is: List[Int], n: Int) ⇒
+      assert(ResourceIterator(is:_*).take(n).toList == is.take(n))
+    }
+  }
+
+  test("iterators created by take should fail when too many elements are requested") {
+    forAll { (is: List[Int], n: Int) ⇒
+      val res = ResourceIterator(is:_*).take(n)
+      while(res.hasNext) res.next()
+      intercept[NoSuchElementException] { res.next() }
+      ()
+    }
+  }
+
+  test("collect should behave as expected") {
+    forAll { (is: List[Int], f: Int ⇒ Option[String]) ⇒
+      val partial = Function.unlift(f)
+      assert(ResourceIterator(is:_*).collect(partial).toList == is.collect(partial))
+    }
+  }
+
+  test("iterators created by collect should fail when too many elements are requested") {
+    forAll { (is: List[Int], f: Int ⇒ Option[String]) ⇒
+      val res = ResourceIterator(is:_*).collect(Function.unlift(f))
+      while(res.hasNext) res.next()
+      intercept[NoSuchElementException] { res.next() }
+      ()
+    }
+  }
+
+  test("map should behave as expected") {
+    forAll { (is: List[Int], f: Int ⇒ String) ⇒
+      assert(ResourceIterator(is:_*).map(f).toList == is.map(f))
+    }
+  }
+
+  test("flatMap should behave as expected") {
+    forAll { (is: List[Int], f: Int ⇒ List[String]) ⇒
+      assert(ResourceIterator(is:_*).flatMap(f.andThen(ss ⇒ ResourceIterator(ss:_*))).toList == is.flatMap(f))
+    }
+  }
+
+  test("filter should behave as expected") {
+    forAll { (is: List[Int], f: Int ⇒ Boolean) ⇒
+      assert(ResourceIterator(is:_*).filter(f).toList == is.filter(f))
+    }
+  }
+
+  test("withFilter should behave as expected") {
+    forAll { (is: List[Int], f: Int ⇒ Boolean) ⇒
+      assert(ResourceIterator(is:_*).withFilter(f).toList == is.iterator.withFilter(f).toList)
+    }
+  }
+
+  test("seq should behave as expected") {
+    forAll { (is: List[Int]) ⇒
+      assert(ResourceIterator(is:_*).seq.toList == is)
+    }
+  }
+
+  test("the empty iterator should have a definite size") {
+    assert(ResourceIterator.empty.hasDefiniteSize)
+  }
+
+  test("the empty iterator should not have a next element") {
+    assert(!ResourceIterator.empty.hasNext)
+  }
+
+  test("the empty iterator should throw when next is called") {
+    intercept[NoSuchElementException] { ResourceIterator.empty.next() }
+    ()
+  }
+
+  test("non-empty iterators should not have a definite size") {
+    forAll { (i: Int, is: List[Int]) ⇒
+      assert(!ResourceIterator(i :: is :_*).hasDefiniteSize)
+    }
+  }
+
+  test("forall should behave as expected") {
+    forAll { (is: List[Int], f: Int ⇒ Boolean) ⇒
+      assert(ResourceIterator(is:_*).forall(f) == is.forall(f))
+    }
+  }
+
+  test("isEmpty should behave as expected") {
+    forAll { (is: List[Int]) ⇒
+      assert(ResourceIterator(is:_*).isEmpty == is.isEmpty)
+    }
+  }
+
+  test("find should behave as expected") {
+    forAll { (is: List[Int], f: Int ⇒ Boolean) ⇒
+      assert(ResourceIterator(is:_*).find(f) == is.find(f))
+    }
+  }
+
+  test("exists should behave as expected") {
+    forAll { (is: List[Int], f: Int ⇒ Boolean) ⇒
+      assert(ResourceIterator(is:_*).exists(f) == is.exists(f))
+    }
+  }
+
+  test("isTraversableAgain should return false") {
+    forAll { (is: List[Int]) ⇒
+      assert(!ResourceIterator(is:_*).isTraversableAgain)
+    }
+  }
+
+  test("toTraversable should behave as expected") {
+    forAll { (is: List[Int]) ⇒
+      assert(ResourceIterator(is:_*).toTraversable.toList == is)
+    }
+  }
+
+  test("toIterator should behave as expected") {
+    forAll { (is: List[Int]) ⇒
+      assert(ResourceIterator(is:_*).toIterator.sameElements(is.toIterator))
+    }
+  }
+
+  test("toStream should behave as expected") {
+    forAll { (is: List[Int]) ⇒
+      assert(ResourceIterator(is:_*).toStream == is.toStream)
+    }
+  }
+
+  test("to[List] should behave as expected") {
+    forAll { (is: List[Int]) ⇒
+      assert(ResourceIterator(is:_*).to[List] == is)
+    }
+  }
+
+  test("withClose should register the close function properly") {
+    forAll { (is: List[Int]) ⇒
+      var closed = false
+      val res = ResourceIterator(is:_*).withClose(() ⇒ {closed = true})
+      while(res.hasNext) res.next()
+      assert(closed)
+    }
+  }
+}
