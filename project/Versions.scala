9e6c55257dfd7b9660edcfe4323c558a02e36e18
Nicolas Rinaudo <nicolas@nrinaudo.com>
2017-12-27 21:52:28 +0100

    Update to scalaz 7.2.18

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,6 +9,6 @@ object Versions {
   val scalacheck          = "1.13.5"
   val scalacheckShapeless = "1.1.7"
   val scalatest           = "3.0.4"
-  val scalaz              = "7.2.16"
+  val scalaz              = "7.2.18"
   val shapeless           = "2.3.2"
 }
