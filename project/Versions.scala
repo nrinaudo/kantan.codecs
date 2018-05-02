7f79a25cd76f90971bbe5c95d32fee00b61cdf20
Nicolas Rinaudo <nicolas@nrinaudo.com>
2018-05-02 12:32:56 +0200

    [#120] Update to refined 0.9.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   val jodaConvert          = "1.9.2"
   val joda                 = "2.9.9"
   val libra                = "0.3.0"
-  val refined              = "0.8.7"
+  val refined              = "0.9.0"
   val scalacheck           = "1.13.5"
   val scalacheckShapeless  = "1.1.8"
   val scalatest            = "3.0.5"
