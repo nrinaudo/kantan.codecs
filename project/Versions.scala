c1a54ab85cf6c44e4790cc1d1dee3b21bad49a9e
Scala Steward <me@scala-steward.org>
2019-06-01 03:23:45 +0200

    Update refined, refined-scalacheck_1.13 to 0.9.7

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val joda                 = "2.10.1"
   val kindProjector        = "0.9.10"
   val libra                = "0.4.0"
-  val refined              = "0.9.3"
+  val refined              = "0.9.7"
   val scalacheck           = "1.13.5"
   val scalacheckShapeless  = "1.1.8"
   val scalatest            = "3.0.5"
