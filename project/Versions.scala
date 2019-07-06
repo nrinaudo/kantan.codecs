6b7e3f4936be568c65a25f3b1101b6986104ece5
Scala Steward <me@scala-steward.org>
2019-07-06 22:39:01 +0200

    Update libra to 0.6.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -9,7 +9,7 @@ object Versions {
   val jodaConvert          = "1.9.2"
   val joda                 = "2.10.1"
   val kindProjector        = "0.9.10"
-  val libra                = "0.4.0"
+  val libra                = "0.6.0"
   val refined              = "0.9.3"
   val scalacheck           = "1.13.5"
   val scalacheckShapeless  = "1.1.8"
