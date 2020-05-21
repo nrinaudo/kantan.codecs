03b41837ec9a2079b90adbd223aa9f90f47f2a32
exoego <ytatsuno.jp@gmail.com>
2020-05-21 17:52:40 +0900

    Drop Scala.js 0.6 to avoid cost of cross buliding

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,12 +1,11 @@
 object Versions {
-  private val isScalaJS06  = Option(System.getenv("SCALAJS_VERSION")).filter(_.nonEmpty).exists(_.startsWith("0.6"))
   val cats                 = "2.1.1"
   val collectionCompat     = "2.1.6"
   val commonsIo            = "2.6"
   val disciplineScalatest  = "1.0.1"
-  val enumeratum           = if (isScalaJS06) "1.6.0" else "1.6.1"
-  val enumeratumScalacheck = if (isScalaJS06) "1.6.0" else "1.6.1"
-  val imp                  = if (isScalaJS06) "0.4.1" else "0.5.0"
+  val enumeratum           = "1.6.1"
+  val enumeratumScalacheck = "1.6.1"
+  val imp                  = "0.5.0"
   val libra                = "0.7.0"
   val refined              = "0.9.14"
   val scalacheck           = "1.14.3"
