e075ba05affddba8fedbec18f9259d14bfa4ca01
Scala Steward <me@scala-steward.org>
2021-01-20 10:47:26 +0100

    Update scalaz-core, ... to 7.3.3

--- project/Versions.scala
+++ project/Versions.scala
@@ -11,6 +11,6 @@ object Versions {
   val scalacheck           = "1.14.3"
   val scalacheckShapeless  = "1.2.5"
   val scalatest            = "3.2.2"
-  val scalaz               = "7.3.2"
+  val scalaz               = "7.3.3"
   val shapeless            = "2.3.3"
 }
