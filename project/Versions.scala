9e1eda465754cca7f5efde30fcd84c174be16751
Scala Steward <me@scala-steward.org>
2021-03-30 04:59:15 +0200

    Update cats-core, cats-laws to 2.5.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
-  val cats                 = "2.2.0"
+  val cats                 = "2.5.0"
   val collectionCompat     = "2.3.0"
   val commonsIo            = "2.8.0"
   val disciplineScalatest  = "2.0.1"
