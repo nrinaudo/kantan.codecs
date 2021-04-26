82d7ab0697089d0f74c50e861ec696233d9dd3f6
Scala Steward <me@scala-steward.org>
2021-04-26 14:38:09 +0200

    Update discipline-scalatest to 2.1.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val cats                 = "2.2.0"
   val collectionCompat     = "2.3.2"
   val commonsIo            = "2.8.0"
-  val disciplineScalatest  = "2.0.1"
+  val disciplineScalatest  = "2.1.4"
   val enumeratum           = "1.6.1"
   val enumeratumScalacheck = "1.6.1"
   val imp                  = "0.5.0"
