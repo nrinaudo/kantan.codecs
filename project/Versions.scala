05946056f5dac859e17f1816c7f75703fdce408f
Nicolas Rinaudo <nicolas@nrinaudo.com>
2021-09-19 16:51:31 +0200

    Update discipline-scalatest 2.1.5

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@ object Versions {
   val cats                 = "2.6.1"
   val collectionCompat     = "2.5.0"
   val commonsIo            = "2.8.0"
-  val disciplineScalatest  = "2.0.1"
+  val disciplineScalatest  = "2.1.5"
   val enumeratum           = "1.7.0"
   val enumeratumScalacheck = "1.7.0"
   val imp                  = "0.5.0"
